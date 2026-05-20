2ba79765b20ff1b59ef75e06b88b4b90b566eced
cryptoirish <99293405+cryptoirish@users.noreply.github.com>
2026-05-20 10:54:04 +0200

    Add Next.js configuration for images and server actions

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "*.public.blob.vercel-storage.com" },
+    ],
+  },
+  experimental: {
+    serverActions: { bodySizeLimit: "10mb" },
+  },
+};
+
+module.exports = nextConfig;
